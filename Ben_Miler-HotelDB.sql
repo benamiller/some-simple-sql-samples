ae01a04ea15e7f66ba428eb4b3194d81db53879e
Hactually Benji <mrbenamiller@gmail.com>
2023-03-10 16:56:18 +0000

    Add clarifying comments

--- Ben_Miler-HotelDB.sql
+++ Ben_Miler-HotelDB.sql
@@ -1,16 +1,28 @@
 -- Ben Miller 2022-03-10
 
+-- Make sure we drop this database if it exists to reset structure
 DROP DATABASE IF EXISTS hotelSoftwareGuild;
 
+-- Recreate the database
 CREATE DATABASE hotelSoftwareGuild;
 
+-- Ensure we are using this database
 USE hotelSoftwareGuild;
 
+/*
+Create an amenities table that maps between
+IDs to amenity types
+This allows for rooms to have many amenities
+*/
 CREATE TABLE Amenities (
 	amenityID INT PRIMARY KEY,
     amenityType VARCHAR(20)
 );
 
+/*
+Create table of guests, each with their own ID, 
+and all related contact information
+*/
 CREATE TABLE Guests (
 	guestID Int Primary Key,
 	firstName VARCHAR(20),
@@ -22,6 +34,13 @@ CREATE TABLE Guests (
     phone VARCHAR(14)
 );
 
+/*
+Create Rooms
+The Type of room is kept here, with the
+standard occupancy and maximum occupancy
+in case a single or double room could
+accommodate a different number of guests
+*/
 CREATE TABLE Rooms (
 	roomNumber Int Primary Key,
     type VARCHAR(15),
@@ -32,6 +51,13 @@ CREATE TABLE Rooms (
     extraPersonPrice Float
 );
 
+/*
+Create Reservations
+The primary key is composite so that one reservation
+can include multiple rooms, while having the same reservationID
+This references the guestID from Guests
+and the roomNumber from Rooms
+*/
 CREATE TABLE Reservations (
 	reservationID Int,
     guestID Int,
@@ -46,6 +72,10 @@ CREATE TABLE Reservations (
     FOREIGN KEY (roomNumber) REFERENCES Rooms (roomNumber)
 );
 
+/*
+Creates an intermediary between Guests to their Reservations
+since one guest may have many reservations at different times
+*/
 CREATE TABLE GuestsToReservations (
 	guestID INT,
     reservationID INt,
@@ -53,6 +83,10 @@ CREATE TABLE GuestsToReservations (
     FOREIGN KEY (reservationID) REFERENCES Reservations (reservationID)
 );
 
+/*
+Creates an intermediary between Rooms to Amenities
+since one room may have many amenities
+*/
 CREATE TABLE RoomsToAmenities (
 	roomNumber INT,
     amenityID INT,
